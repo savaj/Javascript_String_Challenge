0fd73c50694575d9467155ce098323dcfad6a9fa
savaj <savaj@users.noreply.github.com>
2023-03-02 00:21:25 +0530

    Create duplicate_and_unique_val_arr.js

---
+++ duplicate_and_unique_val_arr.js
@@ -0,0 +1,13 @@
+ let number = [1,2,4,5,2,4,5,1,2];
+  
+//For Duplicate
+  let duplicate_city = number.filter((val,i,a) => a.indexOf(val) !== i);
+  let unique = duplicate_city.filter((val,i,a) => a.indexOf(val) === i);
+
+  // Printing the duplicate value
+  console.log(unique);
+
+
+//For Unique val and remove duplicate value
+  let unique = number.filter((val,i,a) => a.indexOf(val) === i);
+console.log(unique);
